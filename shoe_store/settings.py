2773372ad78a2fa01bf0246038c11efee6897924
Creator_Larry <lawrencengugi90@gmail.com>
2025-05-25 12:23:01 +0300

    Switched to SQLite backend

--- shoe_store/settings.py
+++ shoe_store/settings.py
@@ -79,22 +79,30 @@ WSGI_APPLICATION = 'shoe_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         # 'NAME': 'sql3781072',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#         'USER': 'sql3781072',
+#         'PASSWORD': '2UWAYxXllX',
+#         'HOST': 'sql3.freesqldatabase.com',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'charset': 'utf8mb4',  # Ensures proper storage of Unicode characters
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         }
+#     }
+# }
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'sql3781072',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db.sqlite3",
-        'USER': 'sql3781072',
-        'PASSWORD': '2UWAYxXllX',
-        'HOST': 'sql3.freesqldatabase.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',  # Ensures proper storage of Unicode characters
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
